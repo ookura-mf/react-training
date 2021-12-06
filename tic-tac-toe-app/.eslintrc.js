b1aa90661affc3b5992b3c0cce680ae0964422e4
ookura-mf <ookura.keisuke@moneyforward.co.jp>
2021-12-06 20:39:08 +0900

    eslit fix.

--- tic-tac-toe-app/.eslintrc.js
+++ tic-tac-toe-app/.eslintrc.js
@@ -1,32 +1,32 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true,
-  },
-  'extends': [
-    'plugin:react/recommended',
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+      "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
-    'google',
-  ],
-  'globals': {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
-    React: "writable",
-  },
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true,
+    "plugin:react/recommended",
+    ],
+    "globals": {
+      "Atomics": "readonly",
+      "SharedArrayBuffer": "readonly",
+      "React": "writable",
     },
-    'ecmaVersion': 12,
-    'sourceType': 'module',
-  },
-  'plugins': [
-    'react',
-    "react-hooks",
-    '@typescript-eslint',
-  ],
-  'rules': {},
-  'settings': { 'react': { 'version': "detect" } },
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+    },
+    "settings": { "react": { "version": "detect" } },
 };
